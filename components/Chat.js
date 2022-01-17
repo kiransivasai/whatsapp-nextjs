647ffc97008b6b03bbefc64f6578a4c982d2633b
kiransivasai <kiransiva2060@gmail.com>
2022-01-17 20:14:13 +0530

    Added searchbar

---
+++ components/Chat.js
@@ -0,0 +1,13 @@
+import React from "react";
+import styles from "./Chat.module.scss";
+import moment from "moment";
+
+const Chat = () => {
+  return (
+    <div className={styles.Container}>
+      <h1>test</h1>
+    </div>
+  );
+};
+
+export default Chat;
--- components/Sidebar.js
+++ components/Sidebar.js
@@ -3,6 +3,9 @@ import styles from "./Sidebar.module.scss";
 import { Avatar, IconButton } from "@mui/material";
 import ChatIcon from "@mui/icons-material/Chat";
 import CustomVerticalMore from "./CustomVerticalMore";
+import SearchIcon from "@mui/icons-material/Search";
+import NotificationsOffIcon from "@mui/icons-material/NotificationsOff";
+import ArrowForwardIosIcon from "@mui/icons-material/ArrowForwardIos";
 
 const Sidebar = () => {
   return (
@@ -12,6 +15,7 @@ const Sidebar = () => {
           className={styles.avatar}
           src="https://images.pexels.com/photos/415829/pexels-photo-415829.jpeg?auto=compress&cs=tinysrgb&h=750&w=1260"
         />
+
         <div className={styles.iconsGroup}>
           <IconButton>
             <img src="/assets/images/story.svg" />
@@ -20,6 +24,33 @@ const Sidebar = () => {
             <ChatIcon />
           </IconButton>
           <CustomVerticalMore />
+        </div>
+      </div>
+
+      <div className={styles.notification}>
+        <Avatar className={styles.notificationAvatar}>
+          <NotificationsOffIcon style={{ color: "#9de1fe" }} />
+        </Avatar>
+        <div className={styles.notificationText}>
+          <div>Get Notified of New Messages</div>
+          <div style={{ display: "flex", alignItems: "center" }}>
+            <a href="#">
+              <u>Turn on desktop notifications</u>
+            </a>
+            <IconButton>
+              <ArrowForwardIosIcon style={{ width: 15, height: 15 }} />
+            </IconButton>
+          </div>
+        </div>
+      </div>
+
+      <div className={styles.searchChat}>
+        <div className={styles.searchBar}>
+          <SearchIcon />
+          <input
+            placeholder="Search or start new chat"
+            className={styles.searchInput}
+          />
         </div>
       </div>
     </div>
